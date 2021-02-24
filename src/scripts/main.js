6b7d46a8a0e22a6237cbca39d8ab7d3d6dbe7c16
Henrik Hentschel <henrik.hentschel@zeix.com>
2021-02-24 13:27:08 +0100

    main

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,22 +3,6 @@
  */
 
 // imports
-import SingletonExample from './modules/singleton-example.js';
-import RegularExample from './modules/regular-example.js';
-import { MultipleExportClass, MultipleExportConst as xy, getSingletonId } from './modules/multiple-export-example.js';
 
-// use singleton
-const singletonA = SingletonExample;
-const singletonB = SingletonExample;
-console.log(`should be true: ${singletonA.getId() === singletonB.getId()}`); // should be true
-console.log(`should also be true: ${singletonA.getId() === getSingletonId()}`); // should be true (also with singleton used in multiple-export-example)
-
-// use regular
-const regularA = new RegularExample();
-const regularB = new RegularExample();
-console.log(`should be false: ${regularA.getId() === regularB.getId()}`); // should be false
-
-// use multiple exports
-const multipleExportClass = new MultipleExportClass();
-console.log(multipleExportClass.getId());
-console.log(xy);
+import './modules/regular-example';
+import './modules/singleton-example';
