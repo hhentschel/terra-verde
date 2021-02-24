b8f5be824e915c017ad8366d8a697cb412cc6542
Henrik Hentschel <henrik.hentschel@zeix.com>
2021-02-24 13:31:25 +0100

    generiertes

--- web/assets/scripts/main.js
+++ web/assets/scripts/main.js
@@ -1,24 +1,79 @@
-/**
- * Use this file for main application handling and initializing of modules
- */
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+"use strict";
 
-// imports
-import SingletonExample from './modules/singleton-example.js';
-import RegularExample from './modules/regular-example.js';
-import { MultipleExportClass, MultipleExportConst as xy, getSingletonId } from './modules/multiple-export-example.js';
+require("./modules/regular-example");
 
-// use singleton
-const singletonA = SingletonExample;
-const singletonB = SingletonExample;
-console.log(`should be true: ${singletonA.getId() === singletonB.getId()}`); // should be true
-console.log(`should also be true: ${singletonA.getId() === getSingletonId()}`); // should be true (also with singleton used in multiple-export-example)
+require("./modules/singleton-example");
 
-// use regular
-const regularA = new RegularExample();
-const regularB = new RegularExample();
-console.log(`should be false: ${regularA.getId() === regularB.getId()}`); // should be false
+},{"./modules/regular-example":2,"./modules/singleton-example":3}],2:[function(require,module,exports){
+"use strict";
 
-// use multiple exports
-const multipleExportClass = new MultipleExportClass();
-console.log(multipleExportClass.getId());
-console.log(xy);
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var RegularExample = /*#__PURE__*/function () {
+  function RegularExample() {
+    _classCallCheck(this, RegularExample);
+
+    this.id = Math.random();
+  }
+
+  _createClass(RegularExample, [{
+    key: "getId",
+    value: function getId() {
+      return this.id;
+    }
+  }]);
+
+  return RegularExample;
+}();
+
+exports["default"] = RegularExample;
+
+},{}],3:[function(require,module,exports){
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var SingletonExample = /*#__PURE__*/function () {
+  function SingletonExample() {
+    _classCallCheck(this, SingletonExample);
+
+    this.id = Math.random();
+  }
+
+  _createClass(SingletonExample, [{
+    key: "getId",
+    value: function getId() {
+      return this.id;
+    }
+  }]);
+
+  return SingletonExample;
+}(); // export as singleton
+
+
+var _default = new SingletonExample();
+
+exports["default"] = _default;
+
+},{}]},{},[1]);
+
+//# sourceMappingURL=main.js.map
