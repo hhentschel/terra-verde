63fb6c0a1ddf0411e74f00595bf884c89468fb07
Henrik Hentschel <henrik.hentschel@zeix.com>
2021-05-05 17:45:57 +0200

    uebersetzungen

--- translations/de/site.php
+++ translations/de/site.php
@@ -5,11 +5,9 @@ return array(
 "action.showLess" => "weniger anzeigen",
 
 // Main
-"main.title" => "Die Ausgleichskasse & Pensionskasse für Gastgewerbe und Hotellerie",
-"main.orgName" => "GastroSocial",
+"main.title" => "Startseite",
+"main.orgName" => "Terra-Verde",
 
-// Content Header
-"content.header.claim" => "Sozialversicherungen à la carte",
 
 // Hauptnavigation
 "nav.mainNavigation" => "Hauptnavigation",
@@ -31,7 +29,6 @@ return array(
 "nav.mainNavigation.goToStories" => "Zu allen Stories",
 
 // Sprachnavigation
-"nav.languageNavTitle" => "Sprachwahl",
 "nav.overview" => "Übersicht",
 "nav.serviceNavigation" => "Service-Navigation",
 
@@ -41,17 +38,13 @@ return array(
 "footer.telefon" => "Telefon",
 "footer.fax" => "Fax",
 "footer.directto" => "Direkt zu",
-"footer.link.loginwebconnect" => "Login PartnerWeb-connect",
-"footer.link.lohnrechner" => "Lohnrechner Pensionskasse",
-"footer.link.formulare" => "Formulare & Merkblätter",
-"footer.link.corona" => "Alles zu Corona",
 
 // Footernavigation
-"nav.footer.slug.imprint" => "impressum",
+"nav.footer.slug.imprint" => "Impressum",
+"nav.footer.slug.agb" => "AGB",
 "nav.footer.slug.policy" => "datenschutz",
 
 // Servicenavigation
-"nav.service.slug.formLeaflets" => "formulare-merkblaetter",
 "nav.service.slug.contact" => "kontakt",
 "nav.service.slug.search" => "suche",
 
@@ -61,53 +54,16 @@ return array(
 "loginOverlay.loginClose" => "Login Fenster schliessen",
 
 // Categories
-"category.topics" => "Themen",
-"category.language" => "Sprache",
-"category.businessField" => "Geschäftsfeld",
-"category.all" => "Alle",
 
-// Articles (News & Stories)
-"articles.title" => "Gastro Social News",
-"article.goToEntry" => "Zum Beitrag",
-"article.filter.all" => "Alle",
 
 // Formulare
 "form.send" => "Abschicken",
 "form.success" => "Ihre Daten wurden erfolgreich übermittelt. Vielen Dank",
 "form.error" => "Leider ist beim Absenden des Formulars ein Fehler aufgetreten. Bitte versuchen Sie es erneut.",
 
-//Formular und Merkblätter
-"formleaflets.download.text" => "Download",
-"formleaflets.form.text" => "Zum Formular",
-"formleaflets.form.meta.text" => "Formular",
 
-//Further Content
-"furtherContent.Title.DocLinks" => "Weiterführende Informationen und Dokumente",
-"furtherContent.Title.ConsultingSupport" => "Beratung & Support",
-"formleaflets.results.length" => "Ergebnisse entsprechen Ihren Kriterien.",
-"formleaflets.noresults.length" => "Keine Ergebnisse entsprechend Ihren Kriterien gefunden.",
-
-//Further Content
-"furtherContent.Title.DocLinks" => "Dokumente & Links",
-"furtherContent.Title.ConsultingSupport" => "Beratung & Support",
-"furtherContent.Lead.Consulting" => "Wir beraten Sie gerne und sind bei Fragen für Sie da.",
 //Suche
-"search.loadmore.text" => "Mehr laden",
-"search.button.text" => "Suchen",
-"search.no.results" => "Es wurden keine Resultate gefunden.",
-"search.results.length" => "Treffer",
-"search.results.area.emplyoer" => "Arbeitgeber & Selbständige",
-"search.results.area.worker" => "Arbeitnehmende & Pensionierte",
-"search.results.area.aboutUs" => "Über uns Organisation, Jobs & mehr",
-"search.results.area.advisor" => "Ratgeber Basiswissen",
 
-//Pluspoint Liste
-"pluspointlist.title" => "Unsere Pluspunkte",
-"pluspointlist.button" => "Pluspunkte Anzeigen",
-
-"content.openingDaysTitle" => "Montag - Freitag",
-"content.openingDaysAM" => "8:00 - 11:45 Uhr",
-"content.openingDaysPM" => "13:30 - 17:00 Uhr",
 
 "contact.phone.title" => "Telefon",
 "contact.email.title" => "E-Mail"
