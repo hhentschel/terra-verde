502d1898e8f84ce998dc1ffd002277e387e2e7e8
Henrik Hentschel <henrik.hentschel@zeix.com>
2021-05-19 23:01:40 +0200

    shop stuff

--- config/mailchimp-subscribe.php
+++ config/mailchimp-subscribe.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'apiKey' => 'xxxxxxxxxxxxxxxxxxx-us2',
+    'apiKey' => '884a9bcaa4863754f6c539b76035e26f-us13',
     'audienceId' => '7fw6eq98ca',
     'doubleOptIn' => true,
 ];
