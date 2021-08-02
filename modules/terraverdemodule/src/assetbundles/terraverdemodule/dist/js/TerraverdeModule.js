fc6b7111d92db642107c8a888226efe49bc42914
Henrik Hentschel <henrik.hentschel@zeix.com>
2021-08-02 10:44:33 +0200

    new module

---
+++ modules/terraverdemodule/src/assetbundles/terraverdemodule/dist/js/TerraverdeModule.js
@@ -0,0 +1,11 @@
+/**
+ * terraverde module for Craft CMS
+ *
+ * terraverde JS
+ *
+ * @author    Henrik
+ * @copyright Copyright (c) 2021 Henrik
+ * @link      henrik-hentschel.de
+ * @package   TerraverdeModule
+ * @since     1.0.0
+ */
