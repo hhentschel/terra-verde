4f4bd84241a7dfaa8b45a95ab94fa4a7f0e5d9c6
Henrik Hentschel <henrik.hentschel@zeix.com>
2021-08-02 16:38:14 +0200

    yeahh, anpassungen

--- modules/terraverdemodule/src/adjusters/OrderAdjuster.php
+++ modules/terraverdemodule/src/adjusters/OrderAdjuster.php
@@ -24,28 +24,28 @@ class OrderAdjuster extends Component implements AdjusterInterface
 
         $adjustments = [];
 
-        if ($order->itemSubtotal > 100) {
+        if ($order->itemSubtotal > 1000) {
 
                 $adjustment = new OrderAdjustment;
-                $adjustment->type = 'discount';
+                $adjustment->type = 'Rabatt';
                 $adjustment->sourceSnapshot = [ 'data' => 'value' ];
 
 
             // if the amount in the cart is greater than 100$ a discount of 5% is applied.
-            if ($order->itemSubtotal >= 100 && $order->itemSubtotal < 200) {
+            if ($order->itemSubtotal >= 1000 && $order->itemSubtotal < 2500) {
 
                  $adjustment->amount = ($order->itemSubtotal * .05 * -1);
-                 $adjustment->name = 'Order $100 or more';
-                 $adjustment->description = '5% off';
+                 $adjustment->name = 'Bestellung > 1000,- CHF ';
+                 $adjustment->description = '5% Rabatt';
 
             }
 
             // if the amount in the cart is greater than 200$ a discount of 10% is applied.
 
-             if ($order->itemSubtotal >= 200){
+             if ($order->itemSubtotal >= 2500){
                    $adjustment->amount = ($order->itemSubtotal * .10 * -1);
-                   $adjustment->name = 'Order $200 or more';
-                   $adjustment->description = '10% off';
+                   $adjustment->name = 'Bestellung > 1000,- CHF ';
+                   $adjustment->description = '10% Rabatt';
 
 
             }
