8ea6e85e65a164750425772fde32e5fc59ec9a11
Henrik Hentschel <henrik.hentschel@zeix.com>
2021-08-02 15:17:21 +0200

    new path

--- modules/terraverdemodule/adjusters/OrderAdjuster.php
+++ modules/terraverdemodule/adjusters/OrderAdjuster.php
@@ -17,46 +17,46 @@ use craft\commerce\Model;
 //if the amount in the cart is greater than 100$ a discount of 5% is applied.
 //if the amount in the cart is greater than 200$ a discount of 10% is applied.
 
-// class OrderAdjuster extends Component implements AdjusterInterface
-// {
-//         public function adjust(Order $order): array
-//     {
+class OrderAdjuster extends Component implements AdjusterInterface
+{
+        public function adjust(Order $order): array
+    {
 
-//         $adjustments = [];
+        $adjustments = [];
 
-//         if ($order->itemSubtotal > 100) {
+        if ($order->itemSubtotal > 100) {
 
-//                 $adjustment = new OrderAdjustment;
-//                 $adjustment->type = 'discount';
-//                 $adjustment->sourceSnapshot = [ 'data' => 'value' ];
-
-
-//             // if the amount in the cart is greater than 100$ a discount of 5% is applied.
-//             if ($order->itemSubtotal >= 100 && $order->itemSubtotal < 200) {
-
-//                  $adjustment->amount = ($order->itemSubtotal * .05 * -1);
-//                  $adjustment->name = 'Order $100 or more';
-//                  $adjustment->description = '5% off';
-
-//             }
-
-//             // if the amount in the cart is greater than 200$ a discount of 10% is applied.
-
-//              if ($order->itemSubtotal >= 200){
-//                    $adjustment->amount = ($order->itemSubtotal * .10 * -1);
-//                    $adjustment->name = 'Order $200 or more';
-//                    $adjustment->description = '10% off';
+                $adjustment = new OrderAdjustment;
+                $adjustment->type = 'discount';
+                $adjustment->sourceSnapshot = [ 'data' => 'value' ];
 
 
-//             }
-//                     $adjustment->setOrder($order);
+            // if the amount in the cart is greater than 100$ a discount of 5% is applied.
+            if ($order->itemSubtotal >= 100 && $order->itemSubtotal < 200) {
+
+                 $adjustment->amount = ($order->itemSubtotal * .05 * -1);
+                 $adjustment->name = 'Order $100 or more';
+                 $adjustment->description = '5% off';
+
+            }
+
+            // if the amount in the cart is greater than 200$ a discount of 10% is applied.
+
+             if ($order->itemSubtotal >= 200){
+                   $adjustment->amount = ($order->itemSubtotal * .10 * -1);
+                   $adjustment->name = 'Order $200 or more';
+                   $adjustment->description = '10% off';
 
 
-//               $adjustments[] = $adjustment;
-//           }
-//         return $adjustments;
+            }
+                    $adjustment->setOrder($order);
 
 
-//     }
+              $adjustments[] = $adjustment;
+          }
+        return $adjustments;
 
-// }
+
+    }
+
+}
