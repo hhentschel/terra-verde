e151b1d48b2d230eccbb9f778cde73e3e3c38015
Henrik Hentschel <henrik.hentschel@zeix.com>
2021-08-06 23:08:06 +0200

    in app ausblenden

--- modules/cqcontrolpanel/CQControlPanel.php
+++ modules/cqcontrolpanel/CQControlPanel.php
@@ -3,33 +3,33 @@
 // "terraverde\\": "modules/cqcontrolpanel/",
 
 
-// namespace terraverde;
+namespace terraverde;
 
-// use Craft;
-// use craft\web\twig\variables\Cp;
-// use craft\Events\RegisterCpNavItemsEvent;
-// use Yii\base\Module;
-// use Yii\base\Event;
+use Craft;
+use craft\web\twig\variables\Cp;
+use craft\Events\RegisterCpNavItemsEvent;
+use Yii\base\Module;
+use Yii\base\Event;
 
-// class CQControlPanel extends Module
+class CQControlPanel extends Module
 
-// {
-//   public function init()
+{
+  public function init()
 
-//   {
-//     Craft::setAlias( '@cqcontrolpanel', __Dir__);
-//     parent::init();
-//     Event::on(
-//       Cp::class,
-//       Cp::EVENT_REGISTER_CP_NAV_ITEMS,
-//       function (RegisterCpNavItemsEvent $event) {
-//         $event->navItems[] = [
-//           'url' => 'entries/aboutUsStructure',
-//           'label' => 'Ueber uns',
-//           'icon' => '@cqcontrolpanel/web/img'
-//         ];
-//       }
-//     );
-//   }
+  {
+    Craft::setAlias( '@cqcontrolpanel', __Dir__);
+    parent::init();
+    Event::on(
+      Cp::class,
+      Cp::EVENT_REGISTER_CP_NAV_ITEMS,
+      function (RegisterCpNavItemsEvent $event) {
+        $event->navItems[] = [
+          'url' => 'entries/aboutUsStructure',
+          'label' => 'Ueber uns',
+          'icon' => '@cqcontrolpanel/web/img'
+        ];
+      }
+    );
+  }
 
-// }
+}
