8e8264d4d49be182151a4adc3dea4ca2d8e4196b
Henrik Hentschel <henrik.hentschel@zeix.com>
2022-05-16 22:35:22 +0200

    add true composer

---
+++ modules/terraverdemodule/src/adjusters/PickUpDiscountB2B.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace modules\terraverdemodule\adjusters;
+
+use Craft;
+use craft\base\Component;
+use craft\commerce\base\AdjusterInterface;
+use craft\commerce\elements\Order;
+use craft\commerce\models\OrderAdjustment;
+use craft\commerce\Plugin;
+use craft\commerce\Model;
+
+// use craft\commerce\elements\Product;
+// use craft\commerce\elements\Variant;
+// use craft\commerce\services\LineItems;
+
+
+//if the amount in the cart is greater than 100$ a discount of 5% is applied.
+//if the amount in the cart is greater than 200$ a discount of 10% is applied.
+
+class PickUpDiscount extends Component implements AdjusterInterface
+{
+  public function adjust(Order $order): array
+  {
+
+    $adjustments = [];
+
+    if ($order->shippingMethodHandle == 'B2C_pickUpAtStore') {
+
+      $adjustment = new OrderAdjustment;
+      $adjustment->type = 'Rabat';
+      $adjustment->sourceSnapshot = ['data' => 'value'];
+
+      $adjustment->amount = ($order->itemSubtotal * .05 * -1);
+      $adjustment->name = 'Abholung';
+      $adjustment->description = '5%';
+
+      $adjustment->setOrder($order);
+
+      $adjustments[] = $adjustment;
+    }
+    return $adjustments;
+  }
+}
