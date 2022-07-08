0b7dece2bfbdde378c9097293f4a4074bf26f2aa
Henrik Hentschel <henrik.hentschel@zeix.com>
2022-07-08 09:27:32 +0200

    new name for retail adjuster > Rundung

--- modules/retail-pricing/src/adjusters/RoundingAdjuster.php
+++ modules/retail-pricing/src/adjusters/RoundingAdjuster.php
@@ -12,25 +12,25 @@ use craft\commerce\models\OrderAdjustment;
 
 class RoundingAdjuster extends Component implements AdjusterInterface
 {
-    public const CENTS_TO_ROUND_DOWN_TO = 5;
+  public const CENTS_TO_ROUND_DOWN_TO = 5;
 
-    public function adjust(Order $order): array
-    {
-        $factor = 100 / self::CENTS_TO_ROUND_DOWN_TO;
-        $roundedPrice = floor($order->totalPrice * $factor) / $factor;
-        $difference = $roundedPrice - $order->totalPrice;
+  public function adjust(Order $order): array
+  {
+    $factor = 100 / self::CENTS_TO_ROUND_DOWN_TO;
+    $roundedPrice = floor($order->totalPrice * $factor) / $factor;
+    $difference = $roundedPrice - $order->totalPrice;
 
-        if ($difference >= 0) {
-            return [];
-        }
-
-        $adjustment = new OrderAdjustment();
-        $adjustment->type = 'discount';
-        $adjustment->name = 'Retail';
-        $adjustment->sourceSnapshot = ['data' => 'value'];
-        $adjustment->amount = $difference;
-        $adjustment->setOrder($order);
-
-        return [$adjustment];
+    if ($difference >= 0) {
+      return [];
     }
+
+    $adjustment = new OrderAdjustment();
+    $adjustment->type = 'discount';
+    $adjustment->name = 'Rundung';
+    $adjustment->sourceSnapshot = ['data' => 'value'];
+    $adjustment->amount = $difference;
+    $adjustment->setOrder($order);
+
+    return [$adjustment];
+  }
 }
