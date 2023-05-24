7c1eaf3d44fcfd18f9d7c091277b53df348e1251
Henrik Hentschel <henrik.hentschel@zeix.com>
2023-05-24 12:56:42 +0200

    gallery npm flickity

---
+++ src/scripts/modules/gallery.js
@@ -0,0 +1,35 @@
+/*
+import Swiper from 'swiper';
+
+// configure Swiper to use modules
+Swiper.use([Navigation]);
+
+export default class TeaserSlider {
+  element = null;
+
+  constructor(sliderTeaserElement) {
+    this.element = sliderTeaserElement;
+    this.init();
+  }
+
+  init() {
+    const swiperContainer = this.element.querySelector('.swiper-container');
+    new Swiper(swiperContainer, {
+      direction: 'horizontal',
+      slidesPerView: 1,
+      breakpoints: {
+        900: {
+          slidesPerView: 2,
+        },
+        1200: {
+          slidesPerView: 3,
+        },
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  }
+}
+*/
