5df4c0f0cc9f501437c164e9a46bd6c6c1e127a8
Henrik Hentschel <henrik.hentschel@zeix.com>
2023-05-31 14:26:53 +0200

    update composer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,43 +4,43 @@
 
 // imports
 
-import './modules/nav-main'
-import './modules/mobile-nav'
-import './modules/focus'
-import './modules/header'
-import Accordion from './modules/accordion'
-import GallerySlider from './modules/gallery'
+import './modules/nav-main';
+import './modules/mobile-nav';
+import './modules/focus';
+import './modules/header';
+import Accordion from './modules/accordion';
+import GallerySlider from './modules/gallery';
 /* import AnchorLink from './modules/anchor-link'; */
-import AnchorLink from './modules/anchor-link'
-import MapLinkToCircle from './modules/map-linktocircle'
-import MapCircleToLink from './modules/map-circletolink'
+import AnchorLink from './modules/anchor-link';
+import MapLinkToCircle from './modules/map-linktocircle';
+import MapCircleToLink from './modules/map-circletolink';
 
 /* Accordions */
-const accordions = document.querySelectorAll('.accordion')
+const accordions = document.querySelectorAll('.accordion');
 accordions.forEach((accordion) => {
-  new Accordion(accordion)
-})
+  new Accordion(accordion);
+});
 
 /* Anchor links */
-const anchorLinks = document.querySelectorAll('.anchor-link')
+const anchorLinks = document.querySelectorAll('.anchor-link');
 anchorLinks.forEach((anchorLink) => {
-  new AnchorLink(anchorLink)
-})
+  new AnchorLink(anchorLink);
+});
 
 /* Gallery Sliders */
-const gallerySliders = document.querySelectorAll('.gallery-container')
+const gallerySliders = document.querySelectorAll('.gallery-container');
 gallerySliders.forEach((gallery) => {
-  new GallerySlider(gallery)
-})
+  new GallerySlider(gallery);
+});
 
 /* Map links to circle */
-const mapLinkstoCircle = document.querySelectorAll('.map-link')
+const mapLinkstoCircle = document.querySelectorAll('.map-link');
 mapLinkstoCircle.forEach((mapLinkToCircle) => {
-  new MapLinkToCircle(mapLinkToCircle)
-})
+  new MapLinkToCircle(mapLinkToCircle);
+});
 
 /* Map circle to links */
-const mapCirclesToLink = document.querySelectorAll('.producer-circle')
+const mapCirclesToLink = document.querySelectorAll('.producer-circle');
 mapCirclesToLink.forEach((mapCircleToLink) => {
-  new MapCircleToLink(mapCircleToLink)
-})
+  new MapCircleToLink(mapCircleToLink);
+});
