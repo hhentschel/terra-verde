5b37bd9aefeb21ca10b8ba40c7f4e28fa43d0d53
Henrik Hentschel <henrik.hentschel@zeix.com>
2023-08-01 18:20:52 +0200

    serveral changes, mainly address handling

--- translations/de/site.php
+++ translations/de/site.php
@@ -1,73 +1,83 @@
 <?php
 return array(
 // Actions
-"action.showMore" => "mehr anzeigen",
-"action.showLess" => "weniger anzeigen",
+  "action.showMore" => "mehr anzeigen",
+  "action.showLess" => "weniger anzeigen",
 
 // Main
-"main.title" => "Startseite",
-"main.orgName" => "Terra-Verde",
+  "main.title" => "Startseite",
+  "main.orgName" => "Terra-Verde",
 
 
 // Hauptnavigation
-"nav.mainNavigation" => "Hauptnavigation",
-"nav.mainNavigation.employer" => "Arbeitgeber",
-"nav.mainNavigation.independent" => "Selbständige",
-"nav.mainNavigation.Worker" => "Arbeitnehmende",
-"nav.mainNavigation.aboutus" => "Über uns",
-"nav.mainNavigation.organisation" => "Organisation, Jobs & mehr",
-"nav.mainNavigation.retired" => "Pensionierte",
-"nav.mainNavigation.advisor" => "Ratgeber",
-"nav.mainNavigation.basicKnowledge" => "Basiswissen",
-"nav.mainNavigation.slug.employer" => "arbeitgeber",
-"nav.mainNavigation.slug.worker" => "arbeitnehmer",
-"nav.mainNavigation.slug.aboutus" => "ueber-uns",
-"nav.mainNavigation.slug.advisor" => "ratgeber",
-"nav.mainNavigation.menutext" => "Menü",
-"nav.mainNavigation.menuClose" => "Menü schliessen",
-"nav.mainNavigation.home" => "Hauptmenü",
-"nav.mainNavigation.goToStories" => "Zu allen Stories",
+  "nav.mainNavigation" => "Hauptnavigation",
+  "nav.mainNavigation.employer" => "Arbeitgeber",
+  "nav.mainNavigation.independent" => "Selbständige",
+  "nav.mainNavigation.Worker" => "Arbeitnehmende",
+  "nav.mainNavigation.aboutus" => "Über uns",
+  "nav.mainNavigation.organisation" => "Organisation, Jobs & mehr",
+  "nav.mainNavigation.retired" => "Pensionierte",
+  "nav.mainNavigation.advisor" => "Ratgeber",
+  "nav.mainNavigation.basicKnowledge" => "Basiswissen",
+  "nav.mainNavigation.slug.employer" => "arbeitgeber",
+  "nav.mainNavigation.slug.worker" => "arbeitnehmer",
+  "nav.mainNavigation.slug.aboutus" => "ueber-uns",
+  "nav.mainNavigation.slug.advisor" => "ratgeber",
+  "nav.mainNavigation.menutext" => "Menü",
+  "nav.mainNavigation.menuClose" => "Menü schliessen",
+  "nav.mainNavigation.home" => "Hauptmenü",
+  "nav.mainNavigation.goToStories" => "Zu allen Stories",
 
 // Sprachnavigation
-"nav.overview" => "Übersicht",
-"nav.serviceNavigation" => "Service-Navigation",
+  "nav.overview" => "Übersicht",
+  "nav.serviceNavigation" => "Service-Navigation",
 
 // Footer Content
-"footer.address" => "Adresse",
-"footer.contact" => "Kontakt",
-"footer.telefon" => "Telefon",
-"footer.fax" => "Fax",
-"footer.directto" => "Direkt zu",
+  "footer.address" => "Adresse",
+  "footer.contact" => "Kontakt",
+  "footer.telefon" => "Telefon",
+  "footer.fax" => "Fax",
+  "footer.directto" => "Direkt zu",
 
 // Footernavigation
-"nav.footer.slug.imprint" => "Impressum",
-"nav.footer.slug.agb" => "AGB",
-"nav.footer.slug.policy" => "datenschutz",
+  "nav.footer.slug.imprint" => "Impressum",
+  "nav.footer.slug.agb" => "AGB",
+  "nav.footer.slug.policy" => "datenschutz",
 
 // Servicenavigation
-"nav.service.slug.contact" => "kontakt",
-"nav.service.slug.search" => "suche",
+  "nav.service.slug.contact" => "kontakt",
+  "nav.service.slug.search" => "suche",
 
-"nav.loginNavTitle" => "Login",
-"loginOverlay.heading.direct" => "Direkt einloggen bei",
-"loginOverlay.heading.login" => "Noch kein Login",
-"loginOverlay.loginClose" => "Login Fenster schliessen",
+  "nav.loginNavTitle" => "Login",
+  "loginOverlay.heading.direct" => "Direkt einloggen bei",
+  "loginOverlay.heading.login" => "Noch kein Login",
+  "loginOverlay.loginClose" => "Login Fenster schliessen",
 
 // Categories
 
 
 // Formulare
-"form.send" => "Abschicken",
-"form.success" => "Ihre Daten wurden erfolgreich übermittelt. Vielen Dank",
-"form.error" => "Leider ist beim Absenden des Formulars ein Fehler aufgetreten. Bitte versuchen Sie es erneut.",
+  "form.send" => "Abschicken",
+  "form.success" => "Ihre Daten wurden erfolgreich übermittelt. Vielen Dank",
+  "form.error" => "Leider ist beim Absenden des Formulars ein Fehler aufgetreten. Bitte versuchen Sie es erneut.",
 
 
 //Suche
-"search.loadmore.text" => "Mehr laden",
-"search.button.text" => "Suchen",
-"search.no.results" => "Es wurden keine Resultate gefunden.",
-"search.results.length" => "Treffer",
+  "search.loadmore.text" => "Mehr laden",
+  "search.button.text" => "Suchen",
+  "search.no.results" => "Es wurden keine Resultate gefunden.",
+  "search.results.length" => "Treffer",
 
-"contact.phone.title" => "Telefon",
-"contact.email.title" => "E-Mail"
+  "contact.phone.title" => "Telefon",
+  "contact.email.title" => "E-Mail",
+
+  "Country" => "Land",
+  "Select country" => "Auswählen",
+  "Full Name" => "Vorname, Name",
+  "Address 1" => "Adresse",
+  "City" => "Stadt",
+  "Postal Code" => "PLZ",
+  "Organization" => "Organisation",
+  "Address Label" => "Label",
+  "Edit" => "Bearbeiten",
 );
