1b7b6c94a5848163c21da28b678af0836371f9b8
Henrik Hentschel <henrik.hentschel@zeix.com>
2023-12-14 17:40:11 +0100

    check for lightswitch and adjust discount math

--- modules/terraverdemodule/src/adjusters/Discount10.php
+++ modules/terraverdemodule/src/adjusters/Discount10.php
@@ -28,7 +28,9 @@ use yii\base\Event;
 class Discount10 extends Component implements AdjusterInterface
 {
   const ADJUSTMENT_TYPE = 'discount';
-  const DISCOUNT_PRICE = .1;
+  const DISCOUNT_PRICE = 0.1;
+
+  // const DISCOUNTED_PERCENTAGE = 10;
 
   const DISCOUNTED_RETAIL_USER_GROUP_ID = 2;
   const DISCOUNTED_RETAIL_USER_GROUP_PERCENTAGE = 11;
@@ -42,19 +44,20 @@ class Discount10 extends Component implements AdjusterInterface
 
     if ($user && ($user->isInGroup(self::DISCOUNTED_RETAIL_USER_GROUP_ID) || $user->isInGroup(self::DISCOUNTED_RETAIL_USER_GROUP_PERCENTAGE))) {
 
-      $discountField = Craft::$app->getFields()->getFieldByHandle('applyDiscount10Percent6Pieces'); //use Craft
-      $discountFieldColumn = ElementHelper::fieldColumnFromField($discountField); //use craft\helpers\ElementHelper
-      $allowedCategoryIds = Category::find()
-        ->where([$discountFieldColumn => ['=', true]])
-        ->ids();
+      /*      $discountField = Craft::$app->getFields()->getFieldByHandle('applyDiscount10Percent6Pieces'); //use Craft
+            $discountFieldColumn = ElementHelper::fieldColumnFromField($discountField); //use craft\helpers\ElementHelper
+            $allowedCategoryIds = Category::find()
+              ->where([$discountFieldColumn => ['=', true]])
+              ->ids();*/
 
 
-      $lightSwitchField = Craft::$app->getFields()->getFieldByHandle('discount10Available');
+      /*$lightSwitchField = Craft::$app->getFields()->getFieldByHandle('discount10Available');
       $lightSwitchFieldColumn = ElementHelper::fieldColumnFromField($lightSwitchField); //use craft\helpers\ElementHelper
       $lightSwitchFieldColumnStatus = Product::find()
         ->where([$lightSwitchFieldColumn => ['=', true]]);
 
-      Craft::dd($lightSwitchFieldColumnStatus);
+      Craft::dd($lightSwitchFieldColumnStatus);*/
+
 
       // TEST check if lightswitch is true
       /*
@@ -77,38 +80,42 @@ class Discount10 extends Component implements AdjusterInterface
           $categoryID[] = $category->id;
         }
       */
-
+      // $discountPercentage = self::DISCOUNTED_PERCENTAGE;
 
       foreach ($order->lineItems as $lineItem) {
+        $purchasable = $lineItem->getPurchasable(); // Get the Purchasable
+        $product = $purchasable->getProduct(); // Get the Product from the Purchasable
 
-        // Get the associated purchasable (usually a Product)
-        $product = $lineItem->getPurchasable();
+        if ($product->discount10Available) {
+          // Get the associated purchasable (usually a Product)
+          //  $product = $lineItem->getPurchasable();
 
-        /*if (in_array($categoryIdsRelatedToVariant, $allowedCategoryIds)) {*/
+          /*if (in_array($categoryIdsRelatedToVariant, $allowedCategoryIds)) {*/
 
-        /*if ($lineItem->qty >= 6) {*/
-        // Ok, so we've identified that the option exists and needs an Adjustment applied to it. Let's stub the model:
-        $adjustment = new OrderAdjustment([
-          // Don't worry, you can have "positive" discounts, and this handle doesn't show up anywhere in the front-end.
-          'type' => self::ADJUSTMENT_TYPE,
-          // 'name' => 'Gift Wrap',
-          'description' => '10% Rabatt ab 6 Stk. TEST',
-          // Set the price, multiplying by the quantity (or not, your choice!)
-          'amount' => self::DISCOUNT_PRICE * $lineItem->qty,
-          // 'amount' => self::DISCOUNT_PRICE,
-        ]);
+          if ($lineItem->qty >= 6) {
 
-        // Make sure the Adjuster knows what Order and LineItem it's supposed to adjust. This also helps calculate stuff in-memory, prior to saving (especially for new LineItems that don't yet have an ID!):
-        $adjustment->setOrder($order);
-        $adjustment->setLineItem($lineItem);
+            // $discount = $product * ($discountPercentage / 100);
+            // Ok, so we've identified that the option exists and needs an Adjustment applied to it. Let's stub the model:
+            $adjustment = new OrderAdjustment([
+              // Don't worry, you can have "positive" discounts, and this handle doesn't show up anywhere in the front-end.
+              'type' => self::ADJUSTMENT_TYPE,
+              // 'name' => 'Gift Wrap',
+              'description' => '10% Rabatt ab 6 Stk. TEST',
+              // 'amount' => self::DISCOUNT_PRICE * $lineItem->qty,
+              'amount' => round(($order->itemSubtotal * self::DISCOUNT_PRICE * -1) * 100) / 100,
+            ]);
 
-        // Perhaps most importantly! Push that adjustment into the array, so it gets returned:
-        $adjustments[] = $adjustment;
-        /*}*/
-        /*}*/
+            // Make sure the Adjuster knows what Order and LineItem it's supposed to adjust. This also helps calculate stuff in-memory, prior to saving (especially for new LineItems that don't yet have an ID!):
+            $adjustment->setOrder($order);
+            $adjustment->setLineItem($lineItem);
 
+            // Perhaps most importantly! Push that adjustment into the array, so it gets returned:
+            $adjustments[] = $adjustment;
+            /*}*/
+          }
+
+        }
       }
-
       // var_dump($adjustments);
       // Return that array—it may be empty, if nothing matched!
     }
